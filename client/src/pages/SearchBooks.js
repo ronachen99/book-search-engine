df3c64ede2f6b8f2313273c20ed3eb1258d42d5b
Rona C <rona.chen@hotmail.com>
2023-07-10 15:34:09 -0400

    fixed bug

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -115,8 +115,8 @@ const SearchBooks = () => {
         <Row>
           {searchedBooks.map((book) => {
             return (
-              <Col md="4">
-                <Card key={book.bookId} border="dark">
+              <Col key={book.bookId} md="4">
+                <Card border="dark">
                   {book.image ? (
                     <Card.Img
                       src={book.image}
